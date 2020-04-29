b279ba995d0f83fda59decdf1a15c6e18fa141a4
Alvhix <alvhix@gmail.com>
2020-04-29 04:30:22 +0200

    gitignore

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ var model = (function () {
     deleteAll: function () {
       calculator = {
         number: '',
-        storedNumber: '',
+        storedNumber: 0,
         operator: '',
         decimal: false
       }
